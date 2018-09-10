d247a2d3f8c1f2d9b3d04e439212eddd5fcb0c65
Unknown <kimberlyshopkins99@gmail.com>
2018-09-10 16:27:33 -0600

    Character Controller

--- New Unity Project/Assets/Scripts/CharacterMovement.cs
+++ New Unity Project/Assets/Scripts/CharacterMovement.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 
-public class CharacterMovement : MonoBehaviour 
+public class CharacterMovement : MonoBehaviour
 {
-
-	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
 	
-	public FloatData MoveX, MoveY, MoveZ;
-
+	
 	private CharacterController controller;
-	private Vector3 moveDirection = Vector3.zero;
+	public MovePattern Pattern;
+	
 
 	private void Start()
 	{
@@ -22,20 +18,7 @@ public class CharacterMovement : MonoBehaviour
 	}
 
 	void Update() {
-		
-		
-		if (controller.isGrounded)
-		{
-			moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-			//moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-			if (Input.GetButton("Jump"))
-				moveDirection.y = jumpSpeed;
-            
-		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
-		
+		Pattern.Invoke(controller, transform);
+	
 	}
 }
--- New Unity Project/Assets/Scripts/FloatInput.cs
+++ New Unity Project/Assets/Scripts/FloatInput.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FloatInput")]
 public class FloatInput : FloatData
 {
-	
-	
 	public string InputType;
 
 	public override float Value
 	{
-		get { return Input.GetAxis(InputType); }
+		get { return Input.GetAxis(InputType)*value; }
 	}
 
 
