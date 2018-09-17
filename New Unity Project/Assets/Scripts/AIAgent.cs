4db92df978d0fef3ceff8eb8261ac015d0fa18e3
Unknown <kimberlyshopkins99@gmail.com>
2018-09-17 14:18:39 -0600

    AI

---
+++ New Unity Project/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent)) ]
+public class AIAgent : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public Transform Destination;
+    public Transform PostPoint;
+    private Transform finalDestinaiton;
+    
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        finalDestinaiton = transform;
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.transform == Destination)
+            finalDestinaiton = Destination;
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        finalDestinaiton = PostPoint;
+    }
+
+    private void Update()
+    {
+        agent.destination = finalDestinaiton.position;
+    }
+}
