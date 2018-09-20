965b3d4ff367fa34b3eeb06c9514d6d42b0c87a2
Unknown <kimberlyshopkins99@gmail.com>
2018-09-20 15:39:47 -0600

    Added while and foreach loops

--- 2670project01/Assets/Scripts/AIAgent.cs
+++ 2670project01/Assets/Scripts/AIAgent.cs
@@ -1,6 +1,14 @@
-﻿using System.Security.Cryptography;
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.AI;
+
+public enum State
+{
+    Chase,
+    Guard
+};
+
 
 [RequireComponent(typeof(NavMeshAgent)) ]
 public class AIAgent : MonoBehaviour
@@ -10,7 +18,10 @@ public class AIAgent : MonoBehaviour
     public Transform PostPoint;
     private Transform finalDestinaiton;
     public FloatData Speed;
-    
+    public Color CurrentColor;
+    public State CurrentState;
+    public Renderer _renderer;
+  
     
     private void Start()
     {
@@ -18,17 +29,41 @@ public class AIAgent : MonoBehaviour
         agent.speed = Speed.Value;
         agent = GetComponent<NavMeshAgent>();
         finalDestinaiton = transform;
+        _renderer = GetComponent<Renderer>();
     }
 
     private void OnTriggerEnter(Collider obj)
     {
         if (obj.transform == Destination)
+        {
             finalDestinaiton = Destination;
+            CurrentState = State.Chase;
+            UpdateColor();
+        }
     }
 
     private void OnTriggerExit(Collider obj)
     {
         finalDestinaiton = PostPoint;
+        CurrentState = State.Guard;
+        UpdateColor();
+    }
+
+    private void UpdateColor()
+    {
+        switch (CurrentState)
+        {
+            case State.Chase : 
+                print("Chasing");
+                _renderer.material.color = Color.red;
+                //set color to red
+                break;
+            case State.Guard :
+                print("Guarding");
+                _renderer.material.color = Color.black;
+                //set color to black
+                break;
+        }
     }
 
     private void Update()
---
+++ 2670project01/Assets/Scripts/ForEachLoop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+
+public class ForEachLoop : MonoBehaviour
+{
+
+    public List<Color> LightColors;
+    public Light Lt;
+    public Color TargetColor;
+    public Color CurrentColor;
+
+    private void Update()
+    {
+        CurrentColor = Color.Lerp(CurrentColor, TargetColor, Mathf.PingPong(Time.time,0.1f));
+        Lt.color = CurrentColor;
+    }
+
+    IEnumerator Start()
+    {
+        Lt = GetComponent<Light>();
+        while (true)
+        {
+
+
+            foreach (var color in LightColors)
+            {
+
+               // Lt.color = color;
+                TargetColor = color;
+                yield return new WaitForSeconds(3);
+
+            }
+            //yield return new WaitForSeconds(3);
+        }
+    }
+}
