7119478f1d6ac505201646fe45917dd8239622b1
Unknown <kimberlyshopkins99@gmail.com>
2018-11-26 16:51:15 -0700

    AddedStore

---
+++ 2670Project02New/Assets/Scripts/IntData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "IntData")]
+public class IntData : MonoBehaviour
+{
+
+	public int Value;
+}
+
