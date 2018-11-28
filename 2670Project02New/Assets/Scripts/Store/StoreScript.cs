c67285d23482ada4229a513af4802d27cf215ee2
Unknown <kimberlyshopkins99@gmail.com>
2018-11-28 14:15:24 -0700

    Worked on game

---
+++ 2670Project02New/Assets/Scripts/Store/StoreScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class StoreScript : MonoBehaviour
+{
+
+    public Score Cash;
+    public GameObject StoreObject;
+
+    private void Update()
+    {
+        if (Cash.value >= 300)
+        {
+            
+        }
+    }
+}
